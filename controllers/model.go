e8075f50318a4a41532917ad0f54ca70e352062b
Jesslyn Nadya Owen <owenjesslyn@gmail.com>
2022-03-25 02:32:20 +0700

    finish code

--- controllers/model.go
+++ controllers/model.go
@@ -15,28 +15,9 @@ type Product struct {
 	Price int    `json:"price"`
 }
 
-type Transaction struct {
-	ID        int `json:"id"`
-	UserID    int `json:"user id"`
-	ProductID int `json:"product id"`
-	Quantity  int `json:"quantity"`
-}
-
-type DetailTransaction struct {
-	ID          int     `json:"id"`
-	DataUser    User    `json:"user id"`
-	DataProduct Product `json:"product id"`
-	Quantity    int     `json:"quantity"`
-}
-
 type UserResponse struct {
 	Response GeneralResponse `json:"response"`
 	Data     User            `json:"data"`
-}
-
-type UsersResponse struct {
-	Response GeneralResponse `json:"response"`
-	Data     []User          `json:"data"`
 }
 
 type ProductResponse struct {
@@ -47,21 +28,6 @@ type ProductResponse struct {
 type ProductsResponse struct {
 	Response GeneralResponse `json:"response"`
 	Data     []Product       `json:"data"`
-}
-
-type TransactionResponse struct {
-	Response GeneralResponse `json:"response"`
-	Data     Transaction     `json:"data"`
-}
-
-type TransactionsResponse struct {
-	Response GeneralResponse `json:"response"`
-	Data     []Transaction   `json:"data"`
-}
-
-type DetailTransactionsResponse struct {
-	Response GeneralResponse     `json:"response"`
-	Data     []DetailTransaction `json:"data"`
 }
 
 type GeneralResponse struct {
